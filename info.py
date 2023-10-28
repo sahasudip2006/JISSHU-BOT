ce10bfb11b2564832c096b72345bcd80dfba74a3
sahasudip2006 <127957723+sahasudip2006@users.noreply.github.com>
2023-10-28 11:11:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,8 +48,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dahif89943:sudipsaha2006@cluster0.iscxsme.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "dahif89943")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sudipsaha:vNTO1ktUn3oUfoUD@cluster0.m4vfdxm.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "sudipsaha")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
